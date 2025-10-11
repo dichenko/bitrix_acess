97b66826218d0144edd869db7d1c7e445a1e9c94
Dinechko <m@dichenko.ru>
2025-10-11 17:56:02 +0300

    fix env

--- api/pass.ts
+++ api/pass.ts
@@ -8,7 +8,7 @@ import { z } from 'zod';
 import qs from 'qs';
 
 const env = (k: string, d?: string) => {
-  const v = process.env[k] ?? d;
+  const v = process.env[`APP_${k}`] ?? process.env[k] ?? d;
   if (v === undefined) throw new Error(`Missing env: ${k}`);
   return v!;
 };
